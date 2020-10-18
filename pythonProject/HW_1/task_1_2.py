484807b2e20b6ae75661445d9e6347b1052e426d
meeeeeeeh <alexanderkarpovich78@gmail.com>
2020-10-18 03:09:10 +0300

    hw_1

---
+++ pythonProject/HW_1/task_1_2.py
@@ -0,0 +1,4 @@
+x = int(input("Введите число x\n"))
+y = int(input("Введите число y\n"))
+print((abs(x) - abs(y)) / (1 + abs(x) * abs(y)))
+
---
+++ pythonProject/HW_1/task_1_4.py
@@ -0,0 +1,4 @@
+from math import sqrt
+a = int(input("Введите число a\n"))
+b = int(input("Введите число b\n"))
+print("Среднее арифметическое: ", (a + b) / 2, "\nСреднее геометрическое: ", sqrt(a * b))
