3fb3e79d1496a0a3becfc7f78f6e28e1fa988bf4
meeeeeeeh <alexanderkarpovich78@gmail.com>
2020-10-28 08:19:50 +0300

    calculator

--- pythonProject/calc.py
+++ pythonProject/calc.py
@@ -4,20 +4,30 @@ class Parser:
         self.operands = []
         self.functions = []
         self.pos = 0
-    #операторы и приоритет их выполнения (чем больше значение, тем меньше приоритет)
+        self.prev_token = None
+    # операторы и приоритет их выполнения (чем больше значение, тем меньше приоритет)
     operators = {'+': 2, '-': 2, '*': 1, '/': 1, '^': 0}
 
-    #функция проверки операции(+, -, * и т.п.)
+    # функция проверки операции(+, -, * и т.п.)
     @staticmethod
     def is_function(ch):
         return ch in Parser.operators.keys()
 
-    #функция проверки приоритета операции
+    # функция проверки приоритета операции
     @staticmethod
     def func_priority(ch):
         return Parser.operators[ch]
 
-    #функция считывания числа
+    # проверка на число с плавающей точкой
+    @staticmethod
+    def isfloat(num):
+        try:
+            float(num)
+            return True
+        except ValueError:
+            return False
+
+    # функция считывания числа
     def read_number(self):
         result = ''
         ch = self.exp[self.pos]
@@ -28,7 +38,7 @@ class Parser:
             ch = self.exp[self.pos]
         return result
 
-    #выполнение опрераций над операндами
+    # выполнение опрераций над операндами
     def execute_function(self):
         if len(self.operands) < 2:
             return
@@ -48,7 +58,7 @@ class Parser:
         elif f == '^':
             self.operands.append(b ** a)
 
-    #фомирование токена
+    # фомирование токена
     def get_token(self):
         for i in range(self.pos, len(self.exp)):
             ch = self.exp[i]
@@ -59,4 +69,40 @@ class Parser:
                 return ch
         return None
 
+    # сравнивание приоритетов операций
+    def can_pop(self, ch):
+        ch1 = self.functions[-1]
+        if not Parser.is_function(ch1):
+            return False
+        p1 = self.func_priority(ch)
+        p2 = self.func_priority(ch1)
+        return p1 >= p2
 
+    def calc(self):
+        self.pos = 0
+        token = self.get_token()
+
+        while token:
+            if token.isspace():
+                pass
+            elif token.isdigit():
+                self.operands.append(int(token))
+            elif self.isfloat(token):
+                self.operands.append(float(token))
+            elif Parser.is_function(token):
+                while self.can_pop(token):
+                    self.execute_function()
+                self.functions.append(token)
+            elif token == '(':
+                self.functions.append(token)
+            elif token == ')':
+                while self.functions and self.functions[-1] != '(':
+                    self.execute_function()
+                self.functions.pop()
+            self.prev_token = token
+            token = self.get_token()
+        return self.operands[0]
+
+
+stroka = '2 + 4 * 2^2'
+print(Parser(stroka).calc())
